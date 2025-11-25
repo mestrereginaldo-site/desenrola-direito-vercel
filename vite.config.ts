68c37ffe99dba32b0822f4bbaaf4b002be653d27
mestrereginaldo-site <reginaldo.oliveira@catolicasc.edu.br>
2025-11-25 13:38:51 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,11 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
   },
 });
